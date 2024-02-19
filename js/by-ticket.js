32ab97765b9f14b1cf0b4ab46dc7c750d5e7ee7a
Tariqulislamsadek <sadeknoorbd@gmail.com>
2024-02-19 17:13:51 +0600

    button select done

--- js/by-ticket.js
+++ js/by-ticket.js
@@ -2,3 +2,23 @@ function buyTicket() {
     const section2 = document.getElementById('select-ticket');
     section2.scrollIntoView({ behavior: 'smooth' });
   }
+
+//   seat select
+
+// Function to handle button click
+    function handleButtonClick(button) {
+      // Toggle the 'btn-selected' class on the clicked button
+      button.classList.toggle('btn-selected');
+
+      // Update the count of selected buttons
+      updateSelectedCount();
+    }
+
+    // Function to update the count of selected buttons
+    function updateSelectedCount() {
+      const selectedButtons = document.querySelectorAll('.btn-selected');
+      const selectedCount = selectedButtons.length;
+
+      // Display the count in the HTML
+      document.getElementById('selectedCount').textContent = selectedCount;
+    }
