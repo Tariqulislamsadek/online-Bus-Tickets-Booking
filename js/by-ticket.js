9fe73b4421b0b7161c89e80f92de8fae48575b91
Tariqulislamsadek <sadeknoorbd@gmail.com>
2024-02-19 20:02:20 +0600

    complite

--- js/by-ticket.js
+++ js/by-ticket.js
@@ -22,3 +22,47 @@ function buyTicket() {
       // Display the count in the HTML
       document.getElementById('selectedCount').textContent = selectedCount;
     }
+
+    // button disable
+
+    function handleButtonClick(button) {
+        const selectedButtons = document.querySelectorAll('.btn-selected');
+        
+        if (selectedButtons.length < 4 || button.classList.contains('btn-selected')) {
+          // Toggle the 'btn-selected' class on the clicked button
+          button.classList.toggle('btn-selected');
+  
+          // Disable other buttons if four are selected
+          if (selectedButtons.length === 3 && !button.classList.contains('btn-selected')) {
+            disableOtherButtons();
+          }
+  
+          // Update the count of selected buttons
+          updateSelectedCount();
+        }
+      }
+  
+      // Function to update the count of selected buttons
+      function updateSelectedCount() {
+        const selectedButtons = document.querySelectorAll('.btn-selected');
+        const selectedCount = selectedButtons.length;
+  
+        // Display the count in the HTML
+        document.getElementById('selectedCount').textContent = selectedCount;
+      }
+  
+      // Function to disable other buttons
+      function disableOtherButtons() {
+        const allButtons = document.querySelectorAll('.btn');
+        
+        allButtons.forEach(button => {
+          if (!button.classList.contains('btn-selected')) {
+            button.disabled = true;
+            button.classList.add('btn-disabled');
+          }
+        });
+      }
+
+
+ 
+
